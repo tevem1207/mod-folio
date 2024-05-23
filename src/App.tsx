390bfea98a2a31fb7a2961e400c63438b0f0e0dd
황태희 <tevem1207@gmail.com>
2024-05-23 22:39:41 +0900

    [feat] install tailwind css and headless ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,7 +18,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
+        <p className='text-3xl font-bold underline'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
