6757b14632af5aab6cfb978ac1bc09682e0a306d
tevem1207 <tevem1207@gmail.com>
2024-05-25 13:53:14 +0900

    [feat] add block component

--- src/routes/Component.tsx
+++ src/routes/Component.tsx
@@ -1,9 +1,13 @@
 import { Footer, Header } from "@/components";
 
 export const Component = () => {
+  const Blocks = () => {
+    return <div></div>;
+  };
   return (
     <article>
       <Header />
+      <Blocks />
       <Footer />
     </article>
   );
