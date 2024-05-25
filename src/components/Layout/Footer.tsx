dc4be0758f8a03f50db728be88362b43f301739d
tevem1207 <tevem1207@gmail.com>
2024-05-25 13:52:03 +0900

    [feat] set footer content

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,3 +1,10 @@
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <p>© 2024 mod_folio. All rights reserved</p>
+      <div>
+        <button>github</button>
+      </div>
+    </footer>
+  );
 };
