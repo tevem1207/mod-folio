a5f364ecad2a4f70b77761e6fbdadd654c5d0f2b
tevem1207 <tevem1207@gmail.com>
2024-05-25 15:43:15 +0900

    [feat] set eslint import plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,46 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:import/typescript",
+    "prettier",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "postcss.config.js", "tailwind.config.js"],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    ".prettierrc.cjs",
+    "postcss.config.js",
+    "tailwind.config.js",
+  ],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@typescript-eslint", "import"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "no-var": "error",
+    "no-multiple-empty-lines": "error",
+    "no-console": ["warn", { allow: ["error"] }],
+    eqeqeq: "error",
+    "dot-notation": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+        pathGroups: [
+          {
+            pattern: "@/*",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
 };
