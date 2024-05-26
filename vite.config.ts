741eaffdb57892291ea72645b9a702bcae9231cd
tevem1207 <tevem1207@gmail.com>
2024-05-26 16:34:37 +0900

    [feat] add svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths"; // 추가
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths"; // 추가
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
 });
