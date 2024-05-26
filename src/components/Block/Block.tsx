54d6d4ae95e0f3a765707d440f99638fda4fbdc8
tevem1207 <tevem1207@gmail.com>
2024-05-26 18:42:25 +0900

    [feat] add new header and nav menu

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -12,13 +12,13 @@ export const Block = ({ type, data }: BlockProps) => {
 
 export const Blocks = ({ blocksData }: { blocksData?: BlockProps[] }) => {
   return (
-    <div className="relative sm:px-5 md:px-8">
-      <div className="sticky top-0 z-1 py-4 bg-white opacity-90 shadow-sm">
+    <div className="relative">
+      <div className="sticky top-0 z-1 py-4 sm:px-5 md:px-8 bg-white/90 shadow-sm backdrop-blur supports-backdrop-blur:bg-white/95">
         <div className="max-w-screen-lg mx-auto">
           <Menu></Menu>
         </div>
       </div>
-      <div className="max-w-screen-lg mx-auto pt-10 pb-20 flex flex-col gap-5 border-b">
+      <div className="max-w-screen-lg mx-auto sm:px-5 md:px-8 pt-10 pb-20 flex flex-col gap-5 border-b">
         {blocksData?.map((blockdata) => <Block {...blockdata} />)}
       </div>
     </div>
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,11 +1,31 @@
+import { EnvelopeIcon, LinkIcon } from "@heroicons/react/24/outline";
+
+import GithubIcon from "@/assets/github-logo.svg?react";
+
 export const Header = () => {
   return (
-    <header className="sm:px-5 md:px-8 py-4">
-      gdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd
-      gdgd gdgd gdgd gdgd gdgd gdgd gdgdgdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd
-      gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd
-      gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd
-      gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd gdgd
+    <header className="header-background sm:px-5 md:px-8 bg-slate-100">
+      <div className="max-w-screen-lg w-full mx-auto">
+        <div className="text-xl font-bold py-4">ModFolio</div>
+        <div className="pt-4 pb-8">
+          <h2 className="text-3xl font-bold py-4">This is My Portfolio</h2>
+          <p className="opacity-90 py-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div className="h-6 flex gap-4">
+            <button className="size-6 rounded-full p-1 bg-slate-200">
+              <EnvelopeIcon className="size-4" />
+            </button>
+            <button className="size-6 rounded-full p-1 bg-slate-200">
+              <GithubIcon className="size-4" />
+            </button>
+            <button className="size-6 rounded-full p-1 bg-slate-200">
+              <LinkIcon className="size-4" />
+            </button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
